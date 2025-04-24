334d5e129f32c11c015e5e494c89ac74568a043f
Aaron Lai <aklai@student.unimelb.edu.au>
2025-04-25 00:59:03 +1000

    Added local storage persiste with login

--- backend/codebrew/settings/base.py
+++ backend/codebrew/settings/base.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "common",
     "users",
     "produce",
-    #"rest_framework_simplejwt",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        #'rest_framework_simplejwt.authentication.JWTAuthentication',
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
