fbc377fba1a25f6dc3a58ad5e845ac422c7fef02
angchn <angelaisuychen@gmail.com>
2025-04-25 02:49:22 +1000

    Made base product card

--- frontend/js/components/PriceDropdown.tsx
+++ frontend/js/components/PriceDropdown.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ProductCard.css";
+import DownArrow from "../../assets/images/arrow-down-light.svg";
 
 const PriceDropdown = () => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("Default");
+  const items = ["50g", "100g", "150g", "200g"];
+
+  const handleSelect = (item: string) => {
+    setSelected(item);
+    setOpen(false);
+  };
+
   return (
-    <div className="priceDropdownContainer">
-      <select className="priceDropdown">
-        <option>50g</option>
-        <option>100g</option>
-      </select>
+    <div className="dropdownContainer">
+      <div className="dropdownButtonContainer">
+        <div className="dropdownButton" onClick={() => setOpen(!open)}>
+          {selected}
+          <div className="dropdownArrow">
+            <img src={DownArrow} />
+          </div>
+        </div>
+        <div className="price">| $Price</div>
+      </div>
+      {open && (
+        <div className="dropdownMenu">
+          {items.map((item) => (
+            <div
+              key={item}
+              className="dropdownItem"
+              onClick={() => handleSelect(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- frontend/js/components/ProductCard.tsx
+++ frontend/js/components/ProductCard.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import ProductImageWrapper from "./ProductImageWrapper";
 import PriceDropdown from "./PriceDropdown";
+import Button from "./Button";
 
 const ProductCard = () => {
   return (
-    <div>
+    <div style={{ width: "265px" }}>
       <ProductImageWrapper />
       <PriceDropdown />
+      <div className="marginTop">
+        <Button text={"Add to Cart"} handleClick={() => console.log("hi")} />
+      </div>
     </div>
   );
 };
