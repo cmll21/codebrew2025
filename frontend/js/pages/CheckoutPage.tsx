bd475a55ae380d788c8cd9d9365d1e076e21937d
Bec Chao <becchao@gmail.com>
2025-04-25 02:47:14 +1000

    FE Add page messages

--- frontend/js/pages/CheckoutPage.tsx
+++ frontend/js/pages/CheckoutPage.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
 function CheckoutPage() {
-    return (
-        <button>
-                <Link to="/">Go back</Link>
-        </button>
-    )
+  return (
+    <>
+        Look at you saving money and the planet :)
+      <button>
+        <Link to="/">Go back</Link>
+      </button>
+    </>
+  );
 }
 
 export default CheckoutPage;
