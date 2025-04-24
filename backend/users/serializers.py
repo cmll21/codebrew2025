962483a665a027825071c82afd9f3fe2e3bc9c1c
Jacob Ng <ngjacob1@gmail.com>
2025-04-25 02:42:10 +1000

    Rewrote serializers for produce and users with helper classes

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,25 +2,40 @@ from rest_framework import serializers
 from .models import User, ConsumerProfile, SupplierProfile
 
 
-class ConsumerProfileSerializer(serializers.ModelSerializer):
-    #user_info = serializers.PrimaryKeyRelatedField(read_only=True)
-
+class NestedConsumerProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConsumerProfile
         fields = ["delivery_address"]
 
 
-class SupplierProfileSerializer(serializers.ModelSerializer):
-    # user_info = serializers.PrimaryKeyRelatedField(read_only=True)
-
+class NestedSupplierProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = SupplierProfile
         fields = ["location_address"]
 
 
+class NestedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "user_type"]
+
+class ConsumerProfileSerializer(serializers.ModelSerializer):
+    user = NestedUserSerializer(read_only=True)
+
+    class Meta:
+        model = ConsumerProfile
+        fields = ["user", "delivery_address"]
+
+class SupplierProfileSerializer(serializers.ModelSerializer):
+    user = NestedUserSerializer(read_only=True)
+
+    class Meta:
+        model = SupplierProfile
+        fields = ["user", "location_address"]
+
 class UserSerializer(serializers.ModelSerializer):
-    consumer_profile = ConsumerProfileSerializer(read_only=True)
-    supplier_profile = SupplierProfileSerializer(read_only=True)
+    consumer_profile = NestedConsumerProfileSerializer(required=False)
+    supplier_profile = NestedSupplierProfileSerializer(required=False)
 
     class Meta:
         model = User
@@ -43,5 +58,14 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        consumer_data = validated_data.pop("consumer_profile", None)
+        supplier_data = validated_data.pop("supplier_profile", None)
+
         user = User.objects.create_user(**validated_data)
+
+        if user.user_type == "consumer" and consumer_data:
+            ConsumerProfile.objects.create(user=user, **consumer_data)
+        elif user.user_type == "supplier" and supplier_data:
+            SupplierProfile.objects.create(user=user, **supplier_data)
+
         return user
