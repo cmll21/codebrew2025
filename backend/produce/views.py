ee3ecf21e008a20ad7561c4503ad51b4deb209f6
Jacob Ng <ngjacob1@gmail.com>
2025-04-25 18:45:53 +1000

    Added produce categories

--- backend/produce/views.py
+++ backend/produce/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 
-from .models import ProduceItem, ProduceType
-from .serializers import ProduceItemSerializer, ProduceTypeSerializer
+from .models import ProduceItem, ProduceType, ProduceCategory
+from .serializers import ProduceItemSerializer, ProduceTypeSerializer, ProduceCategorySerializer
+
+class ProduceCategoryViewSet(viewsets.ModelViewSet):
+    queryset = ProduceCategory.objects.all()
+    serializer_class = ProduceCategorySerializer
 
 class ProduceTypeViewSet(viewsets.ModelViewSet):
     queryset = ProduceType.objects.all()
