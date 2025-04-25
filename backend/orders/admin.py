09d9e98ae89b4974c49c7bd44b27aea3e62066f7
Jacob Ng <ngjacob1@gmail.com>
2025-04-26 00:09:04 +1000

    Order admin

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
-# from .models import (
-#     OrderStatus,
-#     OrderAddress,
-#     Item,
-#     CartItem,
-#     OrderItem,
-#     Cart,
-#     Order,
-# )
-#
-# admin.site.register(OrderStatus)
-# admin.site.register(OrderAddress)
-# admin.site.register(Item)
-# admin.site.register(CartItem)
-# admin.site.register(OrderItem)
-# admin.site.register(Cart)
-# admin.site.register(Order)
+from .models import (
+    OrderAddress,
+    Item,
+    CartItem,
+    OrderItem,
+    Cart,
+    Order,
+)
+
+admin.site.register(OrderAddress)
+admin.site.register(Item)
+admin.site.register(CartItem)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(Order)
