f16f9a119dec7f2001a59d039c7dad51d993d4ee
angchn <angelaisuychen@gmail.com>
2025-04-25 14:05:45 +1000

    Added seasonal product grid

---
+++ frontend/js/components/SeasonalProduceCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Home.css";
+import "../styles/App.css";
+
+const SeasonalProduceCard = () => {
+  return (
+    <div className="seasonal-produce-card">
+      <h3 className="seasonal-produce-card-label">ProducePlaceholder</h3>
+    </div>
+  );
+};
+
+export default SeasonalProduceCard;
