a707eab275beea786253fc0005a0f5b00902bcc5
Jacob Ng <ngjacob1@gmail.com>
2025-04-26 00:03:59 +1000

    Registered models to admin

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
-# Register your models here.
+# from .models import (
+#     OrderStatus,
+#     OrderAddress,
+#     Item,
+#     CartItem,
+#     OrderItem,
+#     Cart,
+#     Order,
+# )
+#
+# admin.site.register(OrderStatus)
+# admin.site.register(OrderAddress)
+# admin.site.register(Item)
+# admin.site.register(CartItem)
+# admin.site.register(OrderItem)
+# admin.site.register(Cart)
+# admin.site.register(Order)
