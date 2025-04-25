b5560462b672a6a1510703b514d86794276e4736
Bec Chao <becchao@gmail.com>
2025-04-25 22:44:25 +1000

    FE italicised words

--- frontend/js/pages/AboutPage.tsx
+++ frontend/js/pages/AboutPage.tsx
@@ -20,20 +20,24 @@ function AboutPage() {
           <img src={Carrot} alt="Left" className="about-page-carrot-image" />
           <p className="about-page-text">
             We’re here to shake things up and we’re not afraid to get a little
-            wonky doing it. Every year, Aussie farmers are forced to toss out
-            perfectly edible fruits and veggies just because they don’t meet
-            supermarket beauty standards. Twisty carrots, freckled apples, bumpy
-            tomatoes? Still delicious. Still nutritious. Still worthy. We’re
-            building a better way — connecting farmers and wholesalers straight
-            to people who care more about taste than looks. By embracing the
-            weird, we’re making fresh, healthy food more accessible and
-            affordable for everyone. Because food belongs on plates, not in
-            landfills — and the future of food should be full of flavor,
-            fairness, and a little bit of funk.
+            wonky doing it.{" "}
+            <span style={{ fontStyle: "italic" }}>Every year</span>, Aussie
+            farmers are <span style={{ fontStyle: "italic" }}>forced</span> to
+            toss out
+            <span style={{ fontStyle: "italic" }}>
+              perfectly edible fruits and veggies
+            </span>{" "}
+            just because they don’t meet supermarket beauty standards. Twisty
+            carrots, freckled apples, bumpy tomatoes? Still delicious. Still
+            nutritious. Still worthy. We’re building a better way — connecting
+            farmers and wholesalers straight to people who care more about taste
+            than looks. By embracing the weird, we’re making fresh, healthy food
+            more accessible and affordable for everyone. Because food belongs on
+            plates, not in landfills — and the future of food should be full of
+            flavor, fairness, and a little bit of funk.
           </p>
           <img src={Apple} alt="Right" className="about-page-apple-image" />
         </div>
-
       </div>
       <div className="about-page-part2">hi</div>
     </>
