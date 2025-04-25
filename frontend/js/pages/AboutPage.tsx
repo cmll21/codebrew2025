c34a53df8d82336c4b978bf94904505eaa8e674f
Bec Chao <becchao@gmail.com>
2025-04-25 21:17:31 +1000

    FE About page main headers

--- frontend/js/pages/AboutPage.tsx
+++ frontend/js/pages/AboutPage.tsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import AboutPageTitle from "../../assets/images/AboutPageTitle.svg";
+import "../styles/About.css";
 
 function AboutPage() {
-    return (
-        <>
-        Help us defeat food waste!
-      <button>
-        <Link to="/">Go back</Link>
-      </button>
+  return (
+    <>
+      <div className="about-page-part1">
+        <div className="about-page-title">
+          <img src={AboutPageTitle} />
+        </div>
+        <div className="about-page-header">
+        Fighting <span style={{ fontStyle: "italic" }}>Waste</span>&nbsp;with Taste
+        </div>
+      </div>
+      <div className="about-page-part2">
+hi
+      </div>
     </>
-    )
+  );
 }
 
 export default AboutPage;
