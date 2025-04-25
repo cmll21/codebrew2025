67eaf71e6569a80a3f11b2b807466b73fd670253
Jacob Ng <ngjacob1@gmail.com>
2025-04-26 04:29:45 +1000

    Restarted again again

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from .views import CreatePaymentIntent
 
 urlpatterns = [
-    path("create-payment-intent/", CreatePaymentIntent.as_view(), name="create-payment-intent"),
 ]
