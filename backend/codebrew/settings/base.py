a9a91b7066c15f01e416d2515b274129aeabc720
Bec Chao <becchao@gmail.com>
2025-04-26 13:18:48 +1000

    Change page size to 20

--- backend/codebrew/settings/base.py
+++ backend/codebrew/settings/base.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
