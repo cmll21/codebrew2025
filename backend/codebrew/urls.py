f28275a00e3f1927579fd5db497722fc5a523d8f
Jacob Ng <ngjacob1@gmail.com>
2025-04-26 17:34:09 +1000

    Changed media url pattern

--- backend/codebrew/urls.py
+++ backend/codebrew/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, serve
 
 import django_js_reverse.views
 from common.routes import routes as common_routes
@@ -46,8 +46,13 @@ urlpatterns = [
     path("api/register/", RegisterView.as_view(), name="register"),
     path("api/", include(router.urls), name="api"),
     path("payments/", include("payments.urls"), name="payments"),
+    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
 # Serve static and media files in development
 # if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
