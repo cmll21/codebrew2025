f7a849340398e005310a5198e77d4adffb979b2f
Jacob Ng <ngjacob1@gmail.com>
2025-04-26 10:57:34 +1000

    Added swc/wasm

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = (env, argv) => {
           test: /\.(js|mjs|jsx|ts|tsx)$/,
           use: {
             loader: "swc-loader",
+            options: { fallbackToWasm: true },
           },
         },
         {
