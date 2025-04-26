85b33bcea4c9857565c57b4c541ef7ead7d3ebfe
Bec Chao <becchao@gmail.com>
2025-04-26 13:20:06 +1000

    FE Add filtering

--- frontend/js/pages/ShopProducePage.tsx
+++ frontend/js/pages/ShopProducePage.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 import "../styles/ShopProduce.css";
-import { Link } from "react-router-dom";
 import ProduceCardCarousel from "../components/ProduceCardCarousel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const CURR_SEASON = "winter";
+const VEGETABLES = "vegetables";
+const FRUITS = "fruits";
+const LEAFY_GREENS = "leafy_greens";
+const MUSHROOMS = "mushrooms";
+const ROOTS = "roots";
 
 type ProduceType = {
   id: number;
   name: string;
   image: string;
-  category: string | null;
+  category: number;
   season: string | null;
 };
 
+type Category = {
+  id: number;
+  name: string;
+  types: string[];
+}
 const ShopProducePage = () => {
   const [produceItems, setProduceItems] = useState<ProduceType[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]); // May not need this
+  const [categoryMap, setCategoryMap] = useState<Map<number, string>>(new Map());
 
   useEffect(() => {
     fetch("http://localhost:8000/api/produce/types/")
@@ -34,6 +45,26 @@ const ShopProducePage = () => {
       .catch((error) => {
         console.error("Error fetching items:", error);
       });
+  }, []);
+
+  // API Call to finding the categories by ID
+  useEffect(() => {
+    fetch("http://localhost:8000/api/produce/categories/")
+      .then((response) => response.json())
+      .then((data) => {
+        const newCategories = data.results;
+        setCategories(newCategories);
+        
+        // Build the map from the fresh data
+        const newMap = new Map<number, string>();
+        newCategories.forEach((category: Category) => {
+          console.log("category:", category);
+          newMap.set(category.id, category.name);
+          console.log("dsfasda:", newMap.get(category.id));
+        });
+        setCategoryMap(newMap);
+      })
+      .catch(console.error);
   }, []);
 
   return (
@@ -53,7 +84,9 @@ const ShopProducePage = () => {
           <ProduceCardCarousel
             produceList={produceItems
               .filter(item => {
-                console.log("Here:",item.season)
+                if (item.category !== null) {
+                  console.log(`The type of ${item.name} is ${item.category} and ${categoryMap.get(item.category)}`);
+                }
                 return item.season == CURR_SEASON;
               })
               .map((type) => ({
@@ -73,7 +106,11 @@ const ShopProducePage = () => {
           style={{ backgroundColor: "#E2D7C3" }}
         >
           <ProduceCardCarousel
-            produceList={produceItems.map((type) => ({ // Match with category (FK) of each vegetable by doing 2 separate API calls and mapping id to categories
+            produceList={produceItems
+              .filter(item => {
+                return categoryMap.get(item.category) == VEGETABLES;
+              })
+              .map((type) => ({ 
               name: type.name,
               image: type.image,
               cardColour: "#E9B44C",
@@ -90,7 +127,11 @@ const ShopProducePage = () => {
           style={{ backgroundColor: "#F4F1E6" }}
         >
           <ProduceCardCarousel
-            produceList={produceItems.map((type) => ({
+            produceList={produceItems
+              .filter(item => {
+                return categoryMap.get(item.category) == FRUITS;
+              })
+              .map((type) => ({
               name: type.name,
               image: type.image,
               cardColour: "#82A36B",
@@ -107,7 +148,10 @@ const ShopProducePage = () => {
           style={{ backgroundColor: "#E2D7C3" }}
         >
           <ProduceCardCarousel
-            produceList={produceItems.map((type) => ({
+            produceList={produceItems.filter(item => {
+              return categoryMap.get(item.category) == LEAFY_GREENS;
+            })
+              .map((type) => ({
               name: type.name,
               image: type.image,
               cardColour: "#82A36B",
@@ -124,7 +168,11 @@ const ShopProducePage = () => {
           style={{ backgroundColor: "#F4F1E6" }}
         >
           <ProduceCardCarousel
-            produceList={produceItems.map((type) => ({
+            produceList={produceItems
+              .filter(item => {
+                return categoryMap.get(item.category) == MUSHROOMS;
+              })
+              .map((type) => ({
               name: type.name,
               image: type.image,
               cardColour: "#E9B44C",
@@ -141,7 +189,10 @@ const ShopProducePage = () => {
           style={{ backgroundColor: "#E2D7C3" }}
         >
           <ProduceCardCarousel
-            produceList={produceItems.map((type) => ({
+            produceList={produceItems.filter(item => {
+              return categoryMap.get(item.category) == ROOTS;
+            })
+              .map((type) => ({
               name: type.name,
               image: type.image,
               cardColour: "#C87A3F",
